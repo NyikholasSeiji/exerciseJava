6765fba653f211e5586ad2b9e454d3b26de102c0
NyikholasSeiji <seijinyikholas@yahoo.com.br>
2026-08-25 11:59:08 -0300

    test: add and test getters and setters

--- Case Study/3. Cadastro de Alunos/Main.java
+++ Case Study/3. Cadastro de Alunos/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Student student = new Student("Seiji", 20, "ADS");
+        // using getter
+        System.out.println(student.getName());
+        System.out.println(student.getAge());
+        System.out.println(student.getCourse() + "\n");
+        // using setter
+        student.setName("Marcos");
+        student.setAge(21);
+        student.setCourse("IT");
+
         student.display();
     }
 }
--- Case Study/3. Cadastro de Alunos/Student.java
+++ Case Study/3. Cadastro de Alunos/Student.java
@@ -1,17 +1,38 @@
+// create class
 public class Student {
     private String name;
     private int age;
     private String course;
-
+// constructor
     public Student(String name, int age, String course) {
         this.name = name;
         this.age = age;
         this.course = course;
     }
-
+// display
     public void display() {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
         System.out.println("Course: " + course);
+    }
+// getters
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getCourse() {
+        return course;
+    }
+// setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setCourse(String course) {
+        this.course = course;
     }
 }

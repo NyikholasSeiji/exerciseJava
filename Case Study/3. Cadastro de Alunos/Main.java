b36dd2ef7090327bdbbb4acb37d3bdbfe66cb776
NyikholasSeiji <seijinyikholas@yahoo.com.br>
2026-08-25 12:12:23 -0300

    test: practice ArrayList with add and set methods

--- Case Study/3. Cadastro de Alunos/Main.java
+++ Case Study/3. Cadastro de Alunos/Main.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Student student = new Student("Seiji", 20, "ADS");
+        /* Student student = new Student("Seiji", 20, "ADS");
         // using getter
         System.out.println(student.getName());
         System.out.println(student.getAge());
@@ -10,6 +12,24 @@ public class Main {
         student.setAge(21);
         student.setCourse("IT");
 
-        student.display();
+        student.display(); */
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("Seiji", 20, "ADS"));
+        students.add(new Student("Diego", 21, "ADS"));
+        students.add(new Student("Marcos", 22, "IT"));
+
+        for (int i = 0; i < students.size(); i++) {
+            students.get(i).display();
+            System.out.print("\n");
+        }
+
+        students.set(1, new Student("Andre", 18, "Pedagogia"));
+        students.set(2, new Student("Lucas", 19, "ADS"));
+        
+        System.out.print("After set()\n\n");
+        for (int i = 0; i < students.size(); i++) {
+            students.get(i).display();
+            System.out.print("\n");
+        }
     }
 }
